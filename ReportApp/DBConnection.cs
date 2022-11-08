d1a1d27edd8b450911c6bfdb46160cbcd4e9a6ef
Радмир Габдрахманов <gabdrahmanov.radmir2018@gmail.com>
2022-11-08 13:20:26 +0300

    Добавьте файлы проекта.

---
+++ ReportApp/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using ReportApp.ADOApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportApp
+{
+    public static class DBConnection
+    {
+        public static ReportEntities Connection = new ReportEntities();
+    }
+}
---
+++ ReportApp/WindowApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ReportApp.ADOApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReportApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ReportEntities Connection = new ReportEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListDAta.ItemsSource = DBConnection.Connection.Student.ToList();
+        }
+    }
+}
